80f16537dfac789de092e2cac148ed0825ae2c79
hauraarchia <hauraarchia@gmail.com>
2024-09-26 00:13:05 +0700

    praktikum8_js5

---
+++ js05_php-2/gabunganhtml.php
@@ -0,0 +1,10 @@
+<?php
+echo '<html>';
+echo '<head>
+        <title>Cara 01</title>
+    </head>';
+echo '<body>';
+echo '<p>Tanggal hari ini : '.date('d M Y').'</p>';
+echo'</body>';
+echo'</html>';
+?>
