e8fc26521eab043f82e4c912760dce58fefa489b
hauraarchia <hauraarchia@gmail.com>
2024-09-25 16:23:18 +0700

    praktikum1_js5

---
+++ js05_php-2/array_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+    $ListDosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+    
+    foreach ($ListDosen as $Dosen) {
+        echo $Dosen . "<br>";
+    }
+    // echo $ListDosen[2] . "<br>";
+    // echo $ListDosen[0] . "<br>";
+    // echo $ListDosen[1] . "<br>";
+    ?>
+</body>
+</html>
