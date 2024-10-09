a5f9e08373d94c219e17fa203208a5ea13badda7
hauraarchia <hauraarchia@gmail.com>
2024-10-09 12:45:16 +0700

    p3_js7

---
+++ JS07_FormProcessing/form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Form Input PHP</title>
+</head>
+
+<body>
+    <h2>Form Input PHP</h2>
+    <form action="proses_form.php" method="post">
+        <label for="nama">Nama:</label>
+        <input type="text" name="nama" id="nama" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email" required><br><br>
+
+        <input type="submit" value="Submit" name="submit">
+    </form>
+</body>
+
+</html>
