a8eff68d14929f5493c5b21899a9dc6d28fde844
hauraarchia <hauraarchia@gmail.com>
2024-10-16 15:44:29 +0700

    p2.1revisi-js8

--- JS8_FormUpload/proses_upload.php
+++ JS8_FormUpload/proses_upload.php
@@ -1,26 +1,27 @@
 <?php
-//lokasi penyumpanan file yang diunggah
+//lokasi penyimpanan file yang diunggah
 $targetDirectory = "documents/";
 
-//periksa apakah direktori penyimpanan adam jika tidak maka buat
+//periksa apakah direktori penyimpanan ada, jika tidak maka buat
 if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
-    if ($_FILES['files']['name'][0]) {
-        $totalFiles = count($_FILES['files']['name']);
 
-        //loop melalui semua file yang diunggah
-        for ($i = 0; $i < $totalFiles; $i++) {
-            $fileName = $_FILES['files']['name'][$i];
-            $targetFile = $targetDirectory . $fileName;
-        }
+if ($_FILES['files']['name'][0]) {
+    $totalFiles = count($_FILES['files']['name']);
 
-        //pindahkan file yang diunggah ke direktori penyimpanan
+    //loop melalui semua file yang diunggah
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $fileName = $_FILES['files']['name'][$i];
+        $targetFile = $targetDirectory . $fileName;
+
+        //pindahka file yang diunggah ke direktori penyimpanan
         if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
             echo "File $fileName berhasil diunggah.<br>";
         } else {
-            echo "gagal mengunggah file $fileName.<br>";
+            echo "Gagal menggunggah file $fileName.<br>";
         }
-    }else {
+    }
+} else {
     echo "Tidak ada file yang diunggah.";
 }
